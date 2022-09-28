0ff1c430cca879c106353688032e9e112a2ec6d9
GagoBraboH2oh <gabrieljollygamer14@gmail.com>
2022-09-27 23:32:34 -0300

    new

---
+++ formater projects/src/scriptall.js
@@ -0,0 +1,8 @@
+function avisar(){
+    let menuwp = document.getElementById('menu');
+
+    menuwp.style.transform = 'rotate(90deg)'
+    menuwp.style.position = 'fixed'
+    menuwp.style.transition = 'all 3s'
+   
+}
